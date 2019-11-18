42a2812196e8fdeaf1d125de3fb7d2e49fb0a639
Sabrina Pomajulca <sabrina.pomajulca@tecsup.edu.pe>
2019-11-18 15:49:03 -0500

    first commit

---
+++ Laboratorio14/Laboratorio14/ITextToSpeech.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratorio14
+{
+   public  interface ITextToSpeech
+    {
+        void Speak(string text);
+    }
+}
---
+++ Laboratorio14/Laboratorio14/TodoItem.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratorio14
+{
+   public class TodoItem
+    {
+        [PrimaryKey, AutoIncrement]
+
+        public int ID { get; set; }
+        public int Dni { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string FechaNac { get; set; }
+        public bool Done { get; set; }
+
+    }
+}
